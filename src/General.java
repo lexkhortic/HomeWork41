7389de29776cb36e16063219b8dee07fa6441448
lexkhortic <ringostarr1997083@gmail.com>
2022-09-29 20:12:34 +0300

    Replace HW

--- src/General.java
+++ src/General.java
@@ -17,7 +17,7 @@ public class General {
     }
 
     public static String isReliable(String password) {
-        Pattern pattern = Pattern.compile("[a-zA-Z]+\\d+");
+        Pattern pattern = Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}");
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
             return "YES";
